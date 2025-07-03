a958340303a4b92889f8bf88eaa742a651c54fe2
Josh Manzano <32828438+joshmanzano@users.noreply.github.com>
2025-07-03 11:53:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-sudo apt install wget openvpn curl -y \
-wget https://github.com/joshmanzano/xmrig/raw/refs/heads/main/build.zip.gpg \
-gpg --decrypt build.zip.gpg \
-unzip build.zip \
+#!/bin/bash
+
+sudo apt install wget openvpn curl -y
+wget https://github.com/joshmanzano/xmrig/raw/refs/heads/main/build.zip.gpg -o ./build.zip.gpg
+gpg --decrypt ./build.zip.gpg
+unzip ./build.zip
