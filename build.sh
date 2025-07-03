57a64464b486c895482f6870d39aacafb4614821
Josh Manzano <32828438+joshmanzano@users.noreply.github.com>
2025-07-03 13:35:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export MACHINE_ID=$(cat /etc/machine-id 2>/dev/null)
+
+sudo sed -i '/MACHINE_ID/d' /etc/profile
+echo 'export MACHINE_ID=$(cat /etc/machine-id 2>/dev/null)' | sudo tee -a /etc/profile > /dev/null
+
+echo "MACHINE_ID: $MACHINE_ID"
+
 read -sp "Enter passphrase: " PASSPHRASE < /dev/tty
 
 sudo apt install wget openvpn curl build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
@@ -12,7 +19,7 @@ sudo cp ./build/xmrig.service /etc/systemd/system/xmrig.service
 sudo cp ./build/protonvpn-auth.txt /etc/openvpn/client/protonvpn-auth.txt
 sudo cp ./build/tw.protonvpn.tcp.ovpn /etc/openvpn/client/protonvpn.conf
 sudo mkdir /opt/xmrig
-sudo cp -r ./build/xmrig/build/* /opt/xmrig
+sudo cp -r ./build/xmrig/* /opt/xmrig
 
 sudo systemctl daemon-reload
 sudo systemctl enable protonvpn.service
