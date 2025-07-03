6383c513d26326faea471245a256d7f6b3e29821
Josh Manzano <32828438+joshmanzano@users.noreply.github.com>
2025-07-03 12:22:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-read -sp "Enter passphrase: " PASSPHRASE
+read -sp "Enter passphrase: " PASSPHRASE < /dev/tty
 
 sudo apt install wget openvpn curl -y
-wget https://github.com/joshmanzano/xmrig/raw/refs/heads/main/build.gpg -o ./build.gpg
+wget -O ./build.gpg https://github.com/joshmanzano/xmrig/raw/refs/heads/main/build.gpg
 gpg --batch --output build.zip --passphrase $PASSPHRASE --decrypt build.gpg
 unzip ./build.zip
+rm ./build.gpg ./build.zip
+sudo cp ./build/protonvpn.service /etc/systemd/system/protonvpn.service
+sudo cp ./build/xmrig.service /etc/systemd/system/xmrig.service
+sudo cp ./build/protonvpn-auth.txt /etc/openvpn/client/protonvpn-auth.txt
+sudo chmod 600 /etc/openvpn/client/protonvpn-auth.txt
+
+sudo systemctl daemon-reload
+sudo systemctl enable protonvpn.service
+sudo systemctl start protonvpn.service
+sudo systemctl enable xmrig.service
+sudo systemctl start xmrig.service
